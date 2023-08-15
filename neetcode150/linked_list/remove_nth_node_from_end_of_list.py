5f27bac947e28a7c1d7f761e7ad817f90f154fcc
felixng <ngthinh302@gmail.com>
2023-08-15 15:57:49 +0700

    Neetcode 150: Remove Nth Node From End of List

---
+++ neetcode150/linked_list/remove_nth_node_from_end_of_list.py
@@ -0,0 +1,10 @@
+def removeNthFromEnd(head, n):
+    slow, fast = head, head
+    for _ in range(n):
+        fast = fast.next
+    if not fast:
+        return head.next
+    while fast.next:
+        fast, slow = fast.next, slow.next
+    slow.next = slow.next.next
+    return head
